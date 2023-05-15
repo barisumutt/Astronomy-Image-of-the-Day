9beafcd38dae52bdef746b0be4b47ca8228671aa
barisumut <barisumutbaykal@gmail.com>
2023-05-15 21:27:33 +0300

    Reduce the number of codes and edit the explanations

--- Home.py
+++ Home.py
@@ -2,38 +2,33 @@ import streamlit as st
 import requests
 
 
-# Get image from the website
+# Download the image
 image_url = "https://apod.nasa.gov/apod/image/2305/EagleDeep_Lacroce_1080.jpg"
 response = requests.get(image_url)
 
 with open("images/image.jpg", "wb") as file:
     file.write(response.content)
 
-# Get information from the website
+# Prepare API key and API url
 api_key = "zEFBwm5W28FMe4CYcT3Ci5fBPZnAifKPrMLgx3fB"
 url = "https://api.nasa.gov/planetary/apod?" \
       f"api_key={api_key}"
 
-# Make request
+# Get the request data as a dictionay
 request = requests.get(url)
+data = request.json()
 
-# Get a dictionary with data
-content = request.json()
+# Extract the title
+title = data["title"]
 
-# Access the title
-title_body = ""
-for title in content["title"]:
-    title_body = title_body + title
+# Extract the explanation
+explanation = data["explanation"]
 
-# Access the description
-description_body = ""
-for description in content["explanation"]:
-    description_body = description_body + description
-
+# Set the page
 st.set_page_config(layout="wide")
 
-st.title(f"{title_body}")
+st.title(f"{title}")
 st.image("images/image.jpg")
-st.write(f"{description_body}")
+st.write(f"{explanation}")
 
 
